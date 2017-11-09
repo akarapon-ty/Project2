602ef547f14868b2c6021833b4ac4384aa679bf7
Suthawee <suthawee.w@gmail.com>
2017-11-09 12:51:27 +0700

    puadkee

--- app/src/main/java/com/findtheway/GPSTracker.java
+++ app/src/main/java/com/findtheway/GPSTracker.java
@@ -1,36 +1,22 @@
 package com.findtheway;
 
-<<<<<<< HEAD
-=======
 import java.io.IOException;
 import java.util.List;
 import java.util.Locale;
 
->>>>>>> 86532df8cd6847cbc3475ede6a019b648550db4d
 import android.app.AlertDialog;
 import android.app.Service;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
-<<<<<<< HEAD
-=======
 import android.location.Address;
 import android.location.Geocoder;
->>>>>>> 86532df8cd6847cbc3475ede6a019b648550db4d
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.os.IBinder;
 import android.provider.Settings;
-<<<<<<< HEAD
-
-
-/**
- * Created by Apostolis on 10/27/2016.
- */
-
-=======
 import android.util.Log;
 
 /**
@@ -50,81 +36,22 @@ public class GPSTracker extends Service implements LocationListener
 
     //flag for network status
     boolean isNetworkEnabled = false;
->>>>>>> 86532df8cd6847cbc3475ede6a019b648550db4d
 
-public class GPSTracker  extends Service implements LocationListener {
-
-    private final Context context;
-
-    boolean isGPSEnabled = false;
-    boolean isNetworkEnabled = false;
     boolean canGetLocation = false;
 
     Location location;
-
     double latitude;
     double longitude;
 
-<<<<<<< HEAD
-    private static final long MIN_DISTANCE_CHANGE_FOR_UPDATES = 10;
-    private static final long MIN_TIME_BW_UPDATES = 100;
-=======
     //The minimum distance to change updates in metters
     private static final long MIN_DISTANCE_CHANGE_FOR_UPDATES = 10; //10 metters
 
     //The minimum time beetwen updates in milliseconds
     private static final long MIN_TIME_BW_UPDATES = 1000 * 60 * 1; // 1 minute
->>>>>>> 86532df8cd6847cbc3475ede6a019b648550db4d
 
     //Declaring a Location Manager
     protected LocationManager locationManager;
 
-<<<<<<< HEAD
-    public GPSTracker(Context context){
-        this.context = context;
-        getLocation();
-    }
-
-    public Location getLocation(){
-        try {
-            locationManager = (LocationManager)context.getSystemService(LOCATION_SERVICE);
-            isGPSEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
-            isNetworkEnabled = locationManager.isProviderEnabled(locationManager.NETWORK_PROVIDER);
-
-
-            if(!isGPSEnabled && !isGPSEnabled) {
-
-            } else {
-                this.canGetLocation = true;
-
-                if(isNetworkEnabled) {
-                    locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER,
-                            MIN_TIME_BW_UPDATES,
-                            MIN_DISTANCE_CHANGE_FOR_UPDATES, this);
-
-
-                    if (locationManager != null) {
-                        location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
-                        if (location != null) {
-                            latitude = location.getLatitude();
-                            longitude = location.getLongitude();
-
-                        }
-                    }
-                }
-                if(isGPSEnabled) {
-                    if(location == null) {
-                        locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER,
-                                MIN_TIME_BW_UPDATES,
-                                MIN_DISTANCE_CHANGE_FOR_UPDATES, this);
-                        if(locationManager != null) {
-                            location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-                            if(location != null){
-                                latitude = location.getLatitude();
-                                longitude = location.getLongitude();
-
-                            }
-=======
     public GPSTracker(Context context)
     {
         this.mContext = context;
@@ -184,33 +111,20 @@ public class GPSTracker  extends Service implements LocationListener {
                         {
                             location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
                             updateGPSCoordinates();
->>>>>>> 86532df8cd6847cbc3475ede6a019b648550db4d
                         }
-
                     }
-
                 }
             }
-<<<<<<< HEAD
-        } catch (Exception e) {
-            e.printStackTrace();
-=======
         }
         catch (Exception e)
         {
             //e.printStackTrace();
             Log.e("Error : Location", "Impossible to connect to LocationManager", e);
         }
->>>>>>> 86532df8cd6847cbc3475ede6a019b648550db4d
 
-        }
         return location;
     }
 
-<<<<<<< HEAD
-    public void stopUsingGPS(){
-        if(locationManager != null) {
-=======
     public void updateGPSCoordinates()
     {
         if (location != null)
@@ -229,15 +143,10 @@ public class GPSTracker  extends Service implements LocationListener {
     {
         if (locationManager != null)
         {
->>>>>>> 86532df8cd6847cbc3475ede6a019b648550db4d
             locationManager.removeUpdates(GPSTracker.this);
         }
     }
 
-<<<<<<< HEAD
-    public double getLatitude() {
-        if(location != null) {
-=======
     /**
      * Function to get latitude
      */
@@ -245,17 +154,12 @@ public class GPSTracker  extends Service implements LocationListener {
     {
         if (location != null)
         {
->>>>>>> 86532df8cd6847cbc3475ede6a019b648550db4d
             latitude = location.getLatitude();
         }
 
         return latitude;
     }
 
-<<<<<<< HEAD
-    public double getLongitude(){
-        if(location != null) {
-=======
     /**
      * Function to get longitude
      */
@@ -263,61 +167,12 @@ public class GPSTracker  extends Service implements LocationListener {
     {
         if (location != null)
         {
->>>>>>> 86532df8cd6847cbc3475ede6a019b648550db4d
             longitude = location.getLongitude();
         }
 
         return longitude;
     }
 
-<<<<<<< HEAD
-    public boolean canGetLocation() {
-        return this.canGetLocation;
-    }
-
-    public void showSettingsAlert(){
-        AlertDialog.Builder alertDialog = new AlertDialog.Builder(context);
-
-        alertDialog.setTitle("GPS SETTINGS");
-
-        alertDialog.setMessage("GPS is not enabled.Go to Settings menu and enable it?");
-        alertDialog.setPositiveButton("Settings", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
-                context.startActivity(intent);
-            }
-        }) ;
-        alertDialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                dialog.cancel();
-            }
-        });
-        alertDialog.show();
-    }
-
-    @Override
-    public void onLocationChanged(Location location) {
-        this.location = location;
-        getLatitude();
-        getLongitude();
-    }
-
-    @Override
-    public void onStatusChanged(String provider, int status, Bundle extras) {
-
-    }
-
-    @Override
-    public void onProviderEnabled(String provider) {
-
-    }
-
-    @Override
-    public void onProviderDisabled(String provider) {
-
-=======
     /**
      * Function to check GPS/wifi enabled
      */
@@ -485,17 +340,11 @@ public class GPSTracker  extends Service implements LocationListener {
     @Override
     public void onStatusChanged(String provider, int status, Bundle extras)
     {
->>>>>>> 86532df8cd6847cbc3475ede6a019b648550db4d
     }
 
-
     @Override
-<<<<<<< HEAD
-    public IBinder onBind(Intent intent) {
-=======
     public IBinder onBind(Intent intent)
     {
->>>>>>> 86532df8cd6847cbc3475ede6a019b648550db4d
         return null;
     }
 }
--- app/src/main/java/com/findtheway/MapsActivity.java
+++ app/src/main/java/com/findtheway/MapsActivity.java
@@ -1,5 +1,11 @@
 package com.findtheway;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.LocationListener;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.ActivityCompat;
@@ -10,12 +16,23 @@ import android.support.v7.app.ActionBarDrawerToggle;
 import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
+import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.location.Location;
+import android.location.LocationManager;
+import android.widget.TextView;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.app.Activity;
+import java.io.IOException;
+import java.util.List;
 
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -26,10 +43,10 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
-
+    TextView txtLocation;
     private GoogleMap mMap;
-    GPSTracker gps;
 
+GPSTracker gps;
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
         gps = new GPSTracker(MapsActivity.this);
@@ -44,11 +61,11 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         }
     }
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
 
         // setSupportActionBar(toolbar);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
@@ -63,29 +80,19 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             @Override
             public void onClick(View view) {
 
-
                 gps = new GPSTracker(MapsActivity.this);
 
-               if(gps.canGetLocation()){
+                if(gps.canGetLocation()){
 
-<<<<<<< HEAD
-                   double latitude = gps.getLatitude();
-=======
                     double latitude = gps.getLatitude();
->>>>>>> 86532df8cd6847cbc3475ede6a019b648550db4d
                     double longitude = gps.getLongitude();
                     LatLng latLng = new LatLng(latitude,longitude);
-                   mMap.addMarker(new MarkerOptions().position(latLng).title("Me"));
+                    mMap.addMarker(new MarkerOptions().position(latLng).title("Me"));
                     mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
 
 
-<<<<<<< HEAD
-            }else{
-
-=======
                 }else{
                     gps.showSettingsAlert();
->>>>>>> 86532df8cd6847cbc3475ede6a019b648550db4d
                 }
 
 
