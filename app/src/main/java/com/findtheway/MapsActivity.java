c9c1b2a85943ee17e6d5cddc926300002a79c478
Suthawee <suthawee.w@gmail.com>
2017-11-10 10:23:49 +0700

    Fix
    Change Iconforprogramming
    Change GPSTracker check

--- app/src/main/java/com/findtheway/MapsActivity.java
+++ app/src/main/java/com/findtheway/MapsActivity.java
@@ -29,7 +29,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     private GoogleMap mMap;
     GPSTracker gps;
-
     /**
      * Manipulates the map once available.
      * This callback is triggered when the map is ready to be used.
