28a96e7d489a3fe5c039dfaece0c652ce7c564bf
Suthawee <suthawee.w@gmail.com>
2017-11-12 15:25:18 +0700

    Test5

--- app/src/main/java/com/findtheway/MapsActivity.java
+++ app/src/main/java/com/findtheway/MapsActivity.java
@@ -1,167 +1,199 @@
 package com.findtheway;
 
-import android.*;
-import android.Manifest;
-import android.annotation.TargetApi;
-import android.app.AlertDialog;
-import android.content.DialogInterface;
-import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
-import android.os.Build;
-import android.support.annotation.NonNull;
+
+
 import android.support.design.widget.FloatingActionButton;
+
 import android.support.design.widget.NavigationView;
+
 import android.support.v4.app.ActivityCompat;
+
 import android.support.v4.app.FragmentActivity;
+
 import android.os.Bundle;
-import android.support.v4.content.ContextCompat;
+
 import android.support.v4.widget.DrawerLayout;
+
 import android.support.v7.app.ActionBarDrawerToggle;
+
 import android.util.Log;
+
 import android.view.View;
+
 import android.support.design.widget.NavigationView;
+
 import android.support.design.widget.NavigationView;
+
 import android.support.v4.view.GravityCompat;
+
 import android.support.v4.widget.DrawerLayout;
+
 import android.support.v7.app.ActionBarDrawerToggle;
+
 import android.support.v7.app.AppCompatActivity;
+
 import android.support.v7.widget.Toolbar;
 
-import com.google.android.gms.location.FusedLocationProviderClient;
-import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.location.places.GeoDataClient;
-import com.google.android.gms.location.places.PlaceDetectionClient;
-import com.google.android.gms.location.places.Places;
+
+
+
+
 import com.google.android.gms.maps.CameraUpdateFactory;
+
 import com.google.android.gms.maps.GoogleMap;
+
 import com.google.android.gms.maps.OnMapReadyCallback;
+
 import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.CameraPosition;
+
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.Marker;
+
 import com.google.android.gms.maps.model.MarkerOptions;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
 
-import java.util.ArrayList;
 
-import static android.Manifest.permission.ACCESS_COARSE_LOCATION;
-import static android.Manifest.permission.ACCESS_FINE_LOCATION;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
-    private GoogleMap mMap;
-    Marker mMarker;
-    LocationManager lm;
-    double lat, lng;
 
+
+
+    private GoogleMap mMap;
+
+    GPSTracker gps;
 
     /**
+
      * Manipulates the map once available.
+
      * This callback is triggered when the map is ready to be used.
+
      * This is where we can add markers or lines, add listeners or move the camera. In this case,
+
      * we just add a marker near Sydney, Australia.
+
      * If Google Play services is not installed on the device, the user will be prompted to install
+
      * it inside the SupportMapFragment. This method will only be triggered once the user has
+
      * installed Google Play services and returned to the app.
+
      */
+
     public void onMapReady(GoogleMap googleMap) {
+
         mMap = googleMap;
+
+        gps = new GPSTracker(MapsActivity.this);
+
+
+
+        if (gps.canGetLocation()) {
+
+
+
+            double latitude = gps.getLatitude();
+
+            double longitude = gps.getLongitude();
+
+            LatLng latLng = new LatLng(latitude, longitude);
+
+            mMap.addMarker(new MarkerOptions().position(latLng).title("Me"));
+
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+
+
+
+        }
+
+        else {
+
+            Log.d("location", "cannot get location");
+
+        }
+
     }
+
+
 
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+
+
+
+        // setSupportActionBar(toolbar);
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+
                 .findFragmentById(R.id.map);
+
         mapFragment.getMapAsync(this);
+
+
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+
+
+
+            @Override
+
+            public void onClick(View view) {
+
+                gps = new GPSTracker(MapsActivity.this);
+
+                if (gps.canGetLocation()) {
+
+                    double latitude = gps.getLatitude();
+
+                    double longitude = gps.getLongitude();
+
+                    LatLng latLng = new LatLng(latitude, longitude);
+
+                    mMap.addMarker(new MarkerOptions().position(latLng).title("Me"));
+
+                    mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+
+                    Log.d("location after clicked", ""+gps.getLatitude()+","+gps.getLongitude());
+
+                } else {
+
+                    gps.showSettingsAlert();
+
+                }
+
+            }
+
+        });
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+
+        drawer.addDrawerListener(toggle);
+
+        toggle.syncState();
+
+
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+
+//        navigationView.setNavigationItemSelectedListener(this);
+
+
+
+
+
     }
 
-    LocationListener listener = new LocationListener() {
-        public void onLocationChanged(Location loc) {
-            LatLng coordinate = new LatLng(loc.getLatitude()
-                    , loc.getLongitude());
-            lat = loc.getLatitude();
-            lng = loc.getLongitude();
-
-            if (mMarker != null)
-                mMarker.remove();
-
-            mMarker = mMap.addMarker(new MarkerOptions()
-                    .position(new LatLng(lat, lng)));
-            mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(
-                    coordinate, 16));
-        }
-
-        public void onStatusChanged(String provider, int status
-                , Bundle extras) {
-        }
-
-        public void onProviderEnabled(String provider) {
-        }
-
-        public void onProviderDisabled(String provider) {
-        }
-    };
-
-    public void onResume() {
-        super.onResume();
-
-        lm = (LocationManager) getSystemService(this.LOCATION_SERVICE);
-
-        boolean isNetwork =
-                lm.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
-        boolean isGPS =
-                lm.isProviderEnabled(LocationManager.GPS_PROVIDER);
-
-        if (isNetwork) {
-            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                // TODO: Consider calling
-                //    ActivityCompat#requestPermissions
-                // here to request the missing permissions, and then overriding
-                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                //                                          int[] grantResults)
-                // to handle the case where the user grants the permission. See the documentation
-                // for ActivityCompat#requestPermissions for more details.
-                return;
-            }
-            lm.requestLocationUpdates(LocationManager.NETWORK_PROVIDER
-                    , 5000, 10, listener);
-            Location loc = lm.getLastKnownLocation(
-                    LocationManager.NETWORK_PROVIDER);
-            if (loc != null) {
-                lat = loc.getLatitude();
-                lng = loc.getLongitude();
-            }
-        }
-
-        if (isGPS) {
-            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                // TODO: Consider calling
-                //    ActivityCompat#requestPermissions
-                // here to request the missing permissions, and then overriding
-                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                //                                          int[] grantResults)
-                // to handle the case where the user grants the permission. See the documentation
-                // for ActivityCompat#requestPermissions for more details.
-                return;
-            }
-            lm.requestLocationUpdates(LocationManager.GPS_PROVIDER
-                    , 5000, 10, listener);
-            Location loc = lm.getLastKnownLocation(
-                    LocationManager.GPS_PROVIDER);
-            if(loc != null) {
-                lat = loc.getLatitude();
-                lng = loc.getLongitude();
-            }
-        }
-    }
-
-    public void onPause() {
-        super.onPause();
-        lm.removeUpdates(listener);
-    }
 }
